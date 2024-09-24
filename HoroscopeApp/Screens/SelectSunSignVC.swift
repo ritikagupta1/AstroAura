667685477a3172b69e77e1322978564c92f55e08
Ritika Gupta <ritikagupta2020@gmail.com>
2024-09-25 02:11:41 +0530

    Initial Project Setup, Add Logo, launchScreen

--- HoroscopeApp/Screens/SelectSunSignVC.swift
+++ HoroscopeApp/Screens/SelectSunSignVC.swift
@@ -7,10 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SelectSunSignVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .systemPurple
         // Do any additional setup after loading the view.
     }
 
---
+++ HoroscopeApp/SunSignCell.swift
@@ -0,0 +1,19 @@
+//
+//  SunSignCellCollectionViewCell.swift
+//  HoroscopeApp
+//
+//  Created by Ritika Gupta on 25/09/24.
+//
+
+import UIKit
+
+class SunSignCell: UICollectionViewCell {
+    static let reuseIdentifier = "sum"
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
