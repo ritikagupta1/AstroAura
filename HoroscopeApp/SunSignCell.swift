92d3d21129a7d25e8e3e9aaba4278eeb83ee5bec
Ritika Gupta <ritikagupta2020@gmail.com>
2024-09-25 02:11:56 +0530

    Initial Project Setup, Add Logo, launchScreen

--- HoroscopeApp/SunSignCell.swift
+++ HoroscopeApp/SunSignCell.swift
@@ -8,12 +8,55 @@
 import UIKit
 
 class SunSignCell: UICollectionViewCell {
-    static let reuseIdentifier = "sum"
+    static let reuseIdentifier = "sunSignCell"
+    
+    var imageView = UIImageView()
+    var titleLabel = UILabel()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configure()
+        setUpCell(imageName: "AppIcon", title: "Aries")
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    private func configure() {
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        titleLabel.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        
+        imageView.layer.cornerRadius = 20
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        
+        self.contentView.addSubview(imageView)
+        self.contentView.addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 5),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 5),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -5),
+            imageView.heightAnchor.constraint(equalTo: widthAnchor),
+            
+            titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 12),
+            titleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            titleLabel.heightAnchor.constraint(equalToConstant: 25)
+        ])
+    }
+    
+    func setUpCell(imageName: String, title: String) {
+        imageView.image = UIImage(named: imageName)
+        titleLabel.text = title
+    }
+    
+    
+}
+
+
+#Preview {
+    SunSignCell()
 }
